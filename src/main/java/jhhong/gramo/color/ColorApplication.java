a9d6c035a91a2caffdced0bd9c0bbbc433b3e80a
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-07-16 10:00:26 +0900

    :bug: :: Async 버그 수정

--- src/main/java/jhhong/gramo/color/ColorApplication.java
+++ src/main/java/jhhong/gramo/color/ColorApplication.java
@@ -2,9 +2,7 @@ package jhhong.gramo.color;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @SpringBootApplication
 public class ColorApplication {
 
