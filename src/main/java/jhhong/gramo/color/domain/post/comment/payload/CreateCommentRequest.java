6ed5ecbfef269a2f27b5cf1ab26830e1d65c403b
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-19 15:59:43 +0900

    :bug: :: CommentRequest

--- src/main/java/jhhong/gramo/color/domain/post/comment/payload/CreateCommentRequest.java
+++ src/main/java/jhhong/gramo/color/domain/post/comment/payload/CreateCommentRequest.java
@@ -2,12 +2,14 @@ package jhhong.gramo.color.domain.post.comment.payload;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CreateCommentRequest {
     @NotBlank
-    private final String content;
+    private String content;
 }
