a068273eb91611af511f233b819794bd426e5b2b
jhhong0509 <jhhong0509@naver.com>
2021-09-30 19:53:47 +0900

    ⚡️ :: sdf

--- src/main/java/jhhong/gramo/color/domain/post/entity/ReportRepository.java
+++ src/main/java/jhhong/gramo/color/domain/post/entity/ReportRepository.java
@@ -2,8 +2,9 @@ package jhhong.gramo.color.domain.post.entity;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Mono;
 
-public interface ReportRepository extends MongoRepository<Report, ObjectId> {
+public interface ReportRepository extends ReactiveMongoRepository<Report, ObjectId> {
     Mono<Void> deleteAllByPostId(ObjectId postId);
 }
